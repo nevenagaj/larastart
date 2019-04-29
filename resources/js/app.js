a27726f4630d1bd3fc95350973097192b4e88a39
nevenagaj <nevenagajevic@gmail.com>
2019-04-29 23:48:05 +0200

    18. lekcija

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,49 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform';
+import moment from 'moment';
+
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueProgressBar from 'vue-progressbar';
+Vue.use(VueProgressBar,{
+    color: 'rgb(143,255,199)',
+    failedColor: 'red',
+    height: '2px'
+});
+
+import swal from 'sweetalert2';
+window.swal = swal;
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+window.toast = toast;
+
+let routes = [
+    {path: '/dashboard', component: require('./components/Dashboard.vue').default},
+    {path: '/profile', component: require('./components/Profile.vue').default},
+    {path: '/users', component: require('./components/Users.vue').default}
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for 'routes:routes'
+})
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+Vue.filter('myDate', function(created){
+    return moment(created).format('MMMM Do YYYY'); //formatira dutum u koloni datum prijave korisnika
+});
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,4 +72,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
